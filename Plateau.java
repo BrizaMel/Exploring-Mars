3d9fdf132dd958cf1bd85887ba569d1855bfae3b
Briza Mel <brizamel.dias@gmail.com>
2021-12-05 10:58:28 -0300

    Add positionAllowed method to Plateau class
    Signed-off-by: Briza Mel <brizamel.dias@gmail.com>

--- Plateau.java
+++ Plateau.java
@@ -9,4 +9,22 @@ public class Plateau {
         this.row = row;
         this.col = col;
     }
+
+    public Boolean positionAllowed(int x, int y) {
+        Boolean rowInBounds = x >= 0 && x <= this.row;
+        Boolean colInBounds = y >= 0 && y <= this.col;
+        if (rowInBounds || colInBounds) {
+            System.out.println("Position (" + x + "," + y + ") is out of bounds");
+            return false;
+        }
+
+        // check the avaliability of the new position (x, y)
+        String newPos = Integer.toString(x) + ',' + Integer.toString(y);
+        if (this.plateau.contains(newPos)) {
+            System.out.println("Position (" + x + "," + y + ") is already ocuppied");
+            return false;
+        }
+        
+        return true;
+    }
 }
